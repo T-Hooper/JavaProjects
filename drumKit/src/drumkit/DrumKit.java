5a7c3764004b06060ede4e180658a5109e533315
T-Hooper <56169117+T-Hooper@users.noreply.github.com>
2020-03-01 13:07:19 +0000

    Add files via upload

---
+++ drumKit/src/drumkit/DrumKit.java
@@ -0,0 +1,14 @@
+package drumkit;
+
+
+public class DrumKit {
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        Drums x = new Drums();
+        x.setSize(1300, 1000);
+        x.setVisible(true);
+    }
+
+
+}
